0cbcd6876610ca0bd67eb810afd1b59da4b530e5
Michael Patterson <mjpatter88@gmail.com>
2021-08-30 12:21:38 -0500

    Add basic shadows.

--- ray/light.go
+++ ray/light.go
@@ -39,10 +39,19 @@ func computeLighting(position vec3, normal vec3, v vec3, s int) float64 {
 			intensity += light.intensity
 		} else {
 			var lightDirection vec3
+			var tMax float64
 			if light.lightType == "point" {
 				lightDirection = vec3Sub(light.position, position)
+				tMax = 1
 			} else {
 				lightDirection = light.direction
+				tMax = math.Inf(0)
+			}
+
+			// Check if this point is in a shadow relative to this light source
+			shadow_sphere, _ := closestIntersection(position, lightDirection, 0.001, tMax)
+			if shadow_sphere != nil {
+				continue
 			}
 
 			// Diffuse
